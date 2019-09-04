b90c019b488af0b095118403cfc25b3e2ce6596f
Terry Wang <tywang@uchicago.edu>
2019-09-04 17:23:01 -0400

    Added optimized solver

--- knapsack/solver.py
+++ knapsack/solver.py
@@ -4,11 +4,17 @@
 from collections import namedtuple
 
 
-def parse_input(file_location):
-    with open(file_location, 'r') as input_data_file:
-        input_data = input_data_file.read()
+Item = namedtuple("Item", ['index', 'value', 'weight'])
 
-    Item = namedtuple("Item", ['index', 'value', 'weight'])
+
+def solve_it(input_data):
+    """
+
+    :param input_data:
+    :return:
+    """
+    # Modify this code to run your optimization algorithm
+
     # parse the input
     lines = input_data.split('\n')
 
@@ -22,17 +28,6 @@ def parse_input(file_location):
         line = lines[i]
         parts = line.split()
         items.append(Item(i-1, int(parts[0]), int(parts[1])))
-
-    return items, capacity
-
-
-def solve_it(items, capacity):
-    """
-    Solve using sort by value/weight
-    :param items:
-    :param capacity:
-    :return:
-    """
 
     # sort items by value/weight, highest first
     items_sorted = sorted(items, key=lambda x: x[1]/x[2], reverse=True)
@@ -60,9 +55,8 @@ if __name__ == '__main__':
     import sys
     if len(sys.argv) > 1:
         file_location = sys.argv[1].strip()
-        items, capacity = parse_input(file_location)
-
-        # solve the problem
-        print(solve_it(items, capacity))
+        with open(file_location, 'r') as input_data_file:
+            input_data = input_data_file.read()
+        print(solve_it(input_data))
     else:
         print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)')
